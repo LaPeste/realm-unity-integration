e038c7d2fcad7f3d10b3114fe7043cf81817f115
Andrea Catalini <andrea.catalini89@gmail.com>
2020-11-24 17:13:08 +0100

    First working version of realm data linked to the ui

--- Assets/Scripts/UI/InGame/ScoreController.cs
+++ Assets/Scripts/UI/InGame/ScoreController.cs
@@ -1,20 +1,49 @@
 using UnityEngine;  
 using UnityEngine.UI;
+using System;
+using Realms;
 
 namespace Platformer.UI.InGame
 {
+    using Model;
+    using Model.Statistics;
+    using Core;
+
     public class ScoreController : MonoBehaviour
     {
         private Text textLabel;
+        private PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        // just a ref to prevent it from being GCed and not receiveing notification changes
+        private LevelStats _levelStats;
 
         private void Start()
         {
             textLabel = GetComponent<Text>();
+            var realm = Realm.GetInstance(model.RealmConfiguration);
+            var levelName = model.Level.Name;
+            _levelStats = realm.Find<LevelStats>(levelName);
+            textLabel.text = _levelStats.CollectedTokens.ToString();
+
+            _levelStats.PropertyChanged += OnRealmChanged;
         }
-        
+
+        private void OnDestroy()
+        {
+            var realm = Realm.GetInstance(model.RealmConfiguration);
+            realm.RealmChanged -= OnRealmChanged;
+        }
+
         private void Update()
         {
-                        
+                   
+        }
+
+        private void OnRealmChanged(object sender, EventArgs eventArgs)
+        {
+            var realm = Realm.GetInstance(model.RealmConfiguration);
+            var levelName = model.Level.Name;
+            textLabel.text = _levelStats.CollectedTokens.ToString();
         }
     }
 }
